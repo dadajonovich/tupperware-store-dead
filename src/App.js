0e9595843c64a543d96d1b6f161564549ec74c27
Norbaev <timnrbv@gmail.com>
2023-02-24 19:57:49 +0500

    The very beginning of the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+
+function App() {
+  return (
+    <div className="App container mx-auto px-20">
+      <Header />
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+function Categories() {
+  return (
+    <div className="tabs">
+      <a className="tab-bordered tab">Бутылки</a>
+      <a className="tab-bordered tab tab-active">Посуда</a>
+      <a className="tab-bordered tab">Разное</a>
+    </div>
+  );
+}
+
+export default Categories;
